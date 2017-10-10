d6a95e804472abea422a28fab562c0adbd520e2c
Kagamia <amethyst50504724@msn.com>
2017-10-10 21:15:17 +0800

    Version Update

--- WzComparerR2.Network/Properties/AssemblyInfo.cs
+++ WzComparerR2.Network/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.71010")]
