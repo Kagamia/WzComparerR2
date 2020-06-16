2ebc6e2aaea1fe502015ef84296d20b71024b1eb
charlescao460 <charlescao@csr460.com>
2020-06-15 20:06:21 -0400

    Fix NullReferenceException for MapRender

--- WzComparerR2.MapRender/Entry.cs
+++ WzComparerR2.MapRender/Entry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using WzComparerR2.WzLib;
 using WzComparerR2.Common;
@@ -30,6 +31,7 @@ namespace WzComparerR2.MapRender
         private ButtonItem btnItemMapRenderV2;
         private FrmMapRender2 mapRenderGame2;
 
+        [MethodImpl(MethodImplOptions.NoOptimization)]
         protected override void OnLoad()
         {
             #if MapRenderV1
