3dacd6d3a36f4c3819b637607fd23a65a6a148ca
Hyeonmin Park <hyeonmin.park@kennysoft.kr>
2021-03-07 18:33:46 +0900

    LuaConsole: Use UTF-8 Encoding, dependency upgrade. (#152)

--- WzComparerR2.LuaConsole/FrmConsole.cs
+++ WzComparerR2.LuaConsole/FrmConsole.cs
@@ -37,6 +37,7 @@ namespace WzComparerR2.LuaConsole
         private void InitLuaEnv()
         {
             lua = new Lua();
+            lua.State.Encoding = Encoding.UTF8;
             lua.LoadCLRPackage();
             lua["env"] = env;
 
