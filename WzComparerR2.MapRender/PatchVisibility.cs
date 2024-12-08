e3176f2e919b640c9eec9daac77e768621cc224f
PirateIzzy <34289651+PirateIzzy@users.noreply.github.com>
2024-12-08 02:42:16 -0500

    Adding quest and questex options in MapRender (#291)

--- WzComparerR2.MapRender/PatchVisibility.cs
+++ WzComparerR2.MapRender/PatchVisibility.cs
@@ -11,6 +11,8 @@ namespace WzComparerR2.MapRender
         {
             this.dictVisible = new Dictionary<RenderObjectType, bool>();
             this.tagsVisible = new SortedDictionary<string, bool>();
+            this.questVisible = new Dictionary<int, int>();
+            this.questexVisible = new Dictionary<Tuple<int, string>, int>();
             foreach (RenderObjectType type in Enum.GetValues(typeof(RenderObjectType)))
             {
                 this.dictVisible[type] = true;
@@ -100,6 +102,12 @@ namespace WzComparerR2.MapRender
             set { this.SetVisible(RenderObjectType.MobName, value); }
         }
 
+        public bool EffectVisible
+        {
+            get { return IsVisible(RenderObjectType.Effect); }
+            set { this.SetVisible(RenderObjectType.Effect, value); }
+        }
+
         public IReadOnlyDictionary<string, bool> TagsVisible
         {
             get { return this.tagsVisible; }
@@ -109,6 +117,8 @@ namespace WzComparerR2.MapRender
 
         private Dictionary<RenderObjectType, bool> dictVisible;
         private SortedDictionary<string, bool> tagsVisible;
+        private Dictionary<int, int> questVisible;
+        private Dictionary<Tuple<int, string>, int> questexVisible;
 
         public bool IsVisible(RenderObjectType type)
         {
@@ -134,7 +144,7 @@ namespace WzComparerR2.MapRender
 
         public void ResetTagVisible(string[] tags)
         {
-            foreach(var tag in tags)
+            foreach (var tag in tags)
             {
                 this.tagsVisible.Remove(tag);
             }
@@ -143,6 +153,56 @@ namespace WzComparerR2.MapRender
         private void SetVisible(RenderObjectType type, bool visible)
         {
             this.dictVisible[type] = visible;
+        }
+
+        public bool IsVisible(int questID, int questState)
+        {
+            int visible;
+            if (!questVisible.TryGetValue(questID, out visible))
+            {
+                return true;
+            }
+            return visible == -1 || visible == questState;
+        }
+
+        public bool IsVisibleExact(int questID, int questState)
+        {
+            int visible;
+            if (!questVisible.TryGetValue(questID, out visible))
+            {
+                return false;
+            }
+            return visible == questState;
+        }
+
+        public void SetVisible(int questID, int questState)
+        {
+            this.questVisible[questID] = questState;
+        }
+
+        public bool IsVisible(int questID, string qkey, int questState)
+        {
+            int visible;
+            if (!questexVisible.TryGetValue(new Tuple<int, string>(questID, qkey), out visible))
+            {
+                return true;
+            }
+            return visible == -1 || visible == questState;
+        }
+
+        public bool IsVisibleExact(int questID, string qkey, int questState)
+        {
+            int visible;
+            if (!questexVisible.TryGetValue(new Tuple<int, string>(questID, qkey), out visible))
+            {
+                return false;
+            }
+            return visible == questState;
+        }
+
+        public void SetVisible(int questID, string qkey, int questState)
+        {
+            this.questexVisible[new Tuple<int, string>(questID, qkey)] = questState;
         }
     }
 }
