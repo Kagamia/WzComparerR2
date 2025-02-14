49401ec517d376f450a15cae9932a5311e1c3028
Kagamia <amethyst50504724@msn.com>
2025-02-14 17:19:59 +0800

    MainForm: dispose loaded textures before clear in graphics control.

--- WzComparerR2/PictureBoxEx.cs
+++ WzComparerR2/PictureBoxEx.cs
@@ -8,7 +8,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Xna.Framework;
-
+using Microsoft.Xna.Framework.Graphics;
+using SpineV2 = Spine.V2;
 using WzComparerR2.Animation;
 using WzComparerR2.Common;
 using WzComparerR2.Config;
@@ -119,7 +120,12 @@ namespace WzComparerR2
         {
             if (this.Items.Count > 0)
             {
+                var itemsCopy = new List<AnimationItem>(this.Items);
                 this.Items.Clear();
+                foreach (var aniItem in itemsCopy)
+                {
+                    this.DisposeAnimationItem(aniItem);
+                }
             }
 
             this.Items.Add(animator);
@@ -565,6 +571,62 @@ namespace WzComparerR2
                     base.GlobalScale,
                     aniItem.Length <= 0 ? 0 : (time % aniItem.Length),
                     aniItem.Length);
+            }
+        }
+
+        private void DisposeAnimationItem(AnimationItem animationItem)
+        {
+            switch (animationItem)
+            {
+                case FrameAnimator frameAni:
+                    if (frameAni.Data?.Frames != null)
+                    {
+                        foreach (var frame in frameAni.Data.Frames)
+                        {
+                            if (frame.Texture != null && !frame.Texture.IsDisposed)
+                            {
+                                frame.Texture.Dispose();
+                            }
+                        }
+                    }
+                    break;
+                case SpineAnimatorV2 spineV2:
+                    if (spineV2.Skeleton != null)
+                    {
+                        foreach(var slot in spineV2.Skeleton.Slots.Items)
+                        {
+                            var atlasRegion = (slot.Attachment switch
+                            {
+                                SpineV2.MeshAttachment mesh => mesh.RendererObject,
+                                SpineV2.RegionAttachment region => region.RendererObject,
+                                SpineV2.SkinnedMeshAttachment skinnedMesh => skinnedMesh.RendererObject,
+                                _ => null
+                            }) as SpineV2.AtlasRegion;
+                            if (atlasRegion?.page?.rendererObject is Texture2D texture && !texture.IsDisposed)
+                            {
+                                texture.Dispose();
+                            }
+                        }
+                    }
+                    break;
+                case SpineAnimatorV4 spineV4:
+                    if (spineV4.Skeleton != null)
+                    {
+                        foreach (var slot in spineV4.Skeleton.Slots.Items)
+                        {
+                            var atlasRegion = (slot.Attachment switch
+                            {
+                                Spine.MeshAttachment mesh => mesh.Region,
+                                Spine.RegionAttachment region => region.Region,
+                                _ => null
+                            }) as Spine.AtlasRegion;
+                            if (atlasRegion?.page?.rendererObject is Texture2D texture && !texture.IsDisposed)
+                            {
+                                texture.Dispose();
+                            }
+                        }
+                    }
+                    break;
             }
         }
     }
