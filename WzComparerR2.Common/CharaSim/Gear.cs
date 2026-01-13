0303ee3176042e0e9d353ae4e099b675dc49ad09
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2026-01-13 20:29:32 +0800

    Add onmyo sen support

--- WzComparerR2.Common/CharaSim/Gear.cs
+++ WzComparerR2.Common/CharaSim/Gear.cs
@@ -345,6 +345,8 @@ namespace WzComparerR2.CharaSim
                     return GearType.memorialStaff;
                 case 1253:
                     return GearType.celestialLight;
+                case 1254:
+                    return GearType.onmyoSen;
                 case 1259:
                     return GearType.magicStick;
                 case 1403:
